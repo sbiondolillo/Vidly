916d566bfa2cf2bfb1cc32d36fe0b4f713775b7a
Samuel Biondolillo <biwinning121@gmail.com>
2018-01-02 12:18:59 -0500

    add client-side validation and AntiForgeryToken to MovieForm

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -10,19 +10,24 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
         
         public Genre Genre { get; set; }
 
         [Display(Name = "Genre")]
+        [Required]
         public int GenreId { get; set; }
         
         [Display(Name ="Release Date")]
+        [Required]
         public DateTime? ReleaseDate { get; set; }
 
         public DateTime DateAdded { get; set; }
 
         [Display(Name ="Number in Stock")]
+        [Required]
+        [Range(1,20, ErrorMessage = "The field Number in Stock must be between 1 and 20")]
         public int? NumberInStock { get; set; }
 
     }
