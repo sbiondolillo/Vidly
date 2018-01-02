41569c9c47bd133698cca314af454de14005847e
Samuel Biondolillo <biwinning121@gmail.com>
2018-01-02 12:07:02 -0500

    add AntiForgeryToken to MoviesForm

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -75,6 +75,7 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Movie movie)
         {
             if (movie.Id == 0)
