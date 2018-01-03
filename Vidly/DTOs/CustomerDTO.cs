8dd35d59362623852ca4234979654cd97a407d14
Samuel Biondolillo <biwinning121@gmail.com>
2018-01-02 20:37:26 -0500

    add MembershipTypeDTO and utilize in Customer Index view

--- Vidly/DTOs/CustomerDTO.cs
+++ Vidly/DTOs/CustomerDTO.cs
@@ -18,7 +18,9 @@ namespace Vidly.DTOs
         public bool IsSubscribedToNewsletter { get; set; }
 
         public byte MembershipTypeId { get; set; }
-        
+
+        public MembershipTypeDTO MembershipType { get; set; }
+
         //[Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; }
     }
