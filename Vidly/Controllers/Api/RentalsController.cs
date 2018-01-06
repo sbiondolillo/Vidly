4fe22990bed23adce178d68fdd98924b666eee79
Samuel Biondolillo <biwinning121@gmail.com>
2018-01-06 10:04:23 -0500

    begin building Rental API

---
+++ Vidly/Controllers/Api/RentalsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.DTOs;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class RentalsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public RentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // POST: /api/rentals
+        [HttpPost]
+        public IHttpActionResult CreateRental(RentalDTO rentalDTO)
+        {
+            return Ok();
+        }
+
+        
+    }
+}
---
+++ Vidly/DTOs/RentalDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Vidly.DTOs
+{
+    public class RentalDTO
+    {
+        public int CustomerId { get; set; }
+        
+        public List<int> MovieIds { get; set; }
+    }
+}
