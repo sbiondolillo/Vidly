321c3beda5c4fbef3f2f5271bfd80dfd54d3ff89
Samuel Biondolillo <biwinning121@gmail.com>
2018-01-07 22:28:40 -0500

    remove Random action from MoviesController

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -103,23 +103,5 @@ namespace Vidly.Controllers
 
             return RedirectToAction("Index", "Movies");
         }
-        // GET: Movies/Random
-        public ActionResult Random()
-        {
-            var movie = new Movie() { Name = "Shrek!" };
-            var customers = new List<Customer>
-            {
-                new Customer {Name = "Customer 1"},
-                new Customer {Name = "Customer 2"}
-            };
-
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers = customers
-            };
-
-            return View(viewModel);
-        }
     }
 }
